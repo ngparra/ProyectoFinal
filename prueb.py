e3809ceb7ce785dc5661d425670d272924937f67
Nicole Parra <62161098+ngparra@users.noreply.github.com>
2024-12-03 20:27:46 -0600

    Update prueb.py

--- prueb.py
+++ prueb.py
@@ -14,99 +14,79 @@ import branca
 import plotly.graph_objects as go
 import leafmap
 import rasterio
-import io
 import ee
 import geemap
 
-# %%
 from io import BytesIO
-from os import name
 from matplotlib import colors
-from io import BytesIO
 from matplotlib.colors import LinearSegmentedColormap
 from folium.raster_layers import ImageOverlay
-from rasterio.plot import show
-from folium import Choropleth, Popup, Tooltip, GeoJson, GeoJsonTooltip
 from streamlit_folium import folium_static, st_folium
-from folium.raster_layers import ImageOverlay
 from branca.colormap import LinearColormap, linear
 
 # %%
-#CSV Ara Macao y Ara Ambiguus
+# CSV Ara Macao y Ara Ambiguus
 Ara_MacaoAmbiguus = 'Aras/Macao_Ambiguus.csv' 
 
-#Provincias
-lim_provincias= 'Aras/provincias.gpkg'
-
+# Provincias
+lim_provincias = 'Aras/provincias.gpkg'
 
 # %% [markdown]
 # # Carga de datos
 
 # %%
-#carga datos CSV
-
 @st.cache_data
 def cargar_Ara_MacaoAmbiguus():
     MacaoAmbiguus = pd.read_csv(Ara_MacaoAmbiguus)
     return MacaoAmbiguus
 
-# Función para cargar el límite de provincias
-
 @st.cache_data
 def cargar_lim_provincias():
-    provincias= gpd.read_file(lim_provincias)
+    provincias = gpd.read_file(lim_provincias)
     return provincias
-
-
-
 
 # %%
 st.title('Ara Macao y Ara Ambiguus: Costa Rica')
-st.subheader ('Geog. Santiago Brenes Salas (B81292)')
+st.subheader('Geog. Santiago Brenes Salas (B81292)')
 
 # %%
-#Datos de Ara Macao y Ara Ambiguus
+# Datos de Ara Macao y Ara Ambiguus
 carga_MacaoAmbiguus = st.text('Datos de Ara Macao y Ara Ambiguus')
-MacaoAmbiguus_CR = cargar_MacaoAmbiguus()
-carga_MacaoAmbiguus.text('Los datos de Ara Macao y Ara Ambiguus.')
+MacaoAmbiguus_CR = cargar_Ara_MacaoAmbiguus()
+carga_MacaoAmbiguus.text('Los datos de Ara Macao y Ara Ambiguus han sido cargados.')
 
-
-# Cargar datos geoespcailes de las provicnias
+# Cargar datos geoespaciales de las provincias
 carga_provinciasCR = st.text('Cargando datos de los límites de las provincias...')
-provinciasCR = cargar_provincias()
-carga_provinciasCR.text('Los límites de las provincias fueron cargadas.')
-
-
+provinciasCR = cargar_lim_provincias()
+carga_provinciasCR.text('Los límites de las provincias han sido cargados.')
 
 # %%
 MacAmb_prov = MacaoAmbiguus_CR['Provincia'].unique().tolist()
 MacAmb_prov.sort()
 
-#  "Todas" al inicio de la lista
+# "Todas" al inicio de la lista
 opciones_provincias = ['Todas'] + MacAmb_prov
 
 # Selectbox
-provincia_seleccionado = st.sidebar.selectbox(
+provincia_seleccionada = st.sidebar.selectbox(
     'Elige una provincia',
     opciones_provincias
 )
 
-# %%
 # Filtrar datos para no tener nulos
-
-if provincia_seleccionado != 'Todas':
-    # Filtrar los datos para el provincia seleccionado
-    MacaoAmbiguus_filtrados = MacaoAmbiguus_CR[MacaoAmbiguus_CR['Provincia'] == provincia_seleccionado]
-    
-    # Obtener el Código de la provincia seleccionada
+if provincia_seleccionada != 'Todas':
+    MacaoAmbiguus_filtrados = MacaoAmbiguus_CR[MacaoAmbiguus_CR['Provincia'] == provincia_seleccionada]
     Ara_seleccion = MacaoAmbiguus_filtrados['cod_provin'].iloc[0]
 else:
-    # No aplicar filtro
     MacaoAmbiguus_filtrados = MacaoAmbiguus_CR.copy()
-    Ara_seleccion= None
+    Ara_seleccion = None
 
 # %%
-#Unión de los datos de Ara Macao y Ara Ambiguus con las provincias
+# Crear un GeoDataFrame para las provincias
+provinciasCR = provinciasCR.to_crs("EPSG:4326")
+
+# Unión de los datos de Ara Macao y Ara Ambiguus con las provincias
+MacaoAmbiguus_gdf = provinciasCR.copy()
 MacaoAmbiguus_merged = MacaoAmbiguus_gdf.merge(
     MacaoAmbiguus_filtrados, 
     how='inner', 
@@ -114,18 +94,14 @@ MacaoAmbiguus_merged = MacaoAmbiguus_gdf.merge(
     right_on='Provincia'
 )
 
-
-# Filtración de las columnas relevantes del conjunto de los datos 
-    columnas_bf = [
+# Filtración de las columnas relevantes del conjunto de datos
+columnas_bf = [
     'provincia',
     'geometry',
     'Nombre'
 ]
-MacaoAmbiguus_merged= MacaoAmbiguus_merged[columnas_bf]
+MacaoAmbiguus_merged = MacaoAmbiguus_merged[columnas_bf]
 MacaoAmbiguus_merged = MacaoAmbiguus_merged.rename(columns={'Nombre': 'Especie'})
-
-# %%
-# ----- Sección interactiva -----
 
 # Mostrar la tabla
 st.subheader('Datos seleccionables por provincia para Ara Macao y Ara Ambiguus')
@@ -136,7 +112,7 @@ st.dataframe(MacaoAmbiguus_merged, hide_index=True)
 
 # %%
 # Crear una columna con los nombres de los meses
-data['Mes'] = data['month'].replace({
+MacaoAmbiguus_CR['Mes'] = MacaoAmbiguus_CR['month'].replace({
     1: 'Enero', 2: 'Febrero', 3: 'Marzo', 4: 'Abril',
     5: 'Mayo', 6: 'Junio', 7: 'Julio', 8: 'Agosto',
     9: 'Septiembre', 10: 'Octubre', 11: 'Noviembre', 12: 'Diciembre'
@@ -144,95 +120,54 @@ data['Mes'] = data['month'].replace({
 
 # Crear un histograma para la frecuencia de observaciones registradas por mes, separado por especie
 fig = px.histogram(
-    data,
+    MacaoAmbiguus_CR,
     x='Mes',
-    color='Nombre',  # Diferenciar las observaciones por especie
+    color='Nombre',
     title='Frecuencia de observaciones registradas de Ara Macao y Ara Ambiguus por mes',
     labels={
         'Mes': 'Mes',
         'Cantidad de observaciones registradas': 'Cantidad de observaciones registradas'
     },
     color_discrete_map={
-        'Ara macao': '#E60000',     # Color rojo para Ara macao
-        'Ara ambiguus': '#38A800'    # Color verde para Ara ambiguus
+        'Ara macao': '#E60000',
+        'Ara ambiguus': '#38A800'
     }
 )
 
-# Ordenar los meses en el orden cronológico correcto
 fig.update_xaxes(categoryorder='array', categoryarray=[
     'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
     'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
 ])
 
-# Personalizar el diseño del histograma
 fig.update_layout(
     xaxis_title='Mes',
     yaxis_title='Cantidad de observaciones registradas',
-    barmode='group',  # Mostrar las barras de cada especie una al lado de la otra
-    width=1000,       # Ancho del gráfico en píxeles
-    height=600        # Alto del gráfico en píxeles
+    barmode='group',
+    width=1000,
+    height=600
 )
 
-# Actualizar el formato del eje y evitar notación científica
 fig.update_yaxes(tickformat=",d")
-
-# Atributos globales de la figura
-fig.update_layout(
-    legend_title_text='Especie'
-)
-
-# Mostrar el gráfico
-fig.show()
+fig.update_layout(legend_title_text='Especie')
+st.plotly_chart(fig)
 
 # %% [markdown]
 # # Cantidad de observaciones registradas de Ara Macao y Ara Ambiguus por provincia
 
 # %%
-# # # ----- Mapa interactivo: Cantones con presencia de botaderos y su relación con el IDHD -----
-
-MacaoAmbiguus_merged = MacaoAmbiguus_merged.to_crs("EPSG:4326")
-provincias = provincias.to_crs("EPSG:4326")
-
 # Crear el mapa interactivo con las áreas de conservación
-m = polygons_wgs84_gdf.explore(
+m = provinciasCR.explore(
     column='Conteo',
     name='Cantidad de Lapas por provincia',
     cmap='OrRd',
-    tooltip=['PROVINCIA', 'Conteo'],
+    tooltip=['provincia', 'geometry'],
     legend=True,
     legend_kwds={
         'caption': "Distribución de los lapas en Provincias",
         'orientation': "horizontal"
-    },
+    }
 )
-
-# Ruta del ráster remoto
-raster_url = 'https://github.com/gf0657-programacionsig/2024-ii/raw/refs/heads/main/datos/worldclim/altitud.tif'
-
-# Crear una capa de ráster con folium
-raster_layer = folium.raster_layers.TileLayer(
-    tiles=raster_url,
-    attr="WorldClim Altitud",
-    name="Altitud",
-    overlay=True,
-    control=True
-)
-
-# Agregar la capa ráster al mapa
-raster_layer.add_to(m)
-
-# Agregar un control de capas al mapa
-folium.LayerControl().add_to(m)
 
 # Mostrar el mapa interactivo
-m
-
-
-# ----- Agregar control de capas -----
-folium.LayerControl().add_to(mapa)
-
-# ----- Mostrar el mapa en Streamlit -----
-st.subheader('Relación entre los cantones con presencia de botaderos y su promedio del IDHD entre 2010 y 2020 según provincia')
-st_folium(mapa, width=700, height=600)
-
-
+st.subheader('Distribución de Ara Macao y Ara Ambiguus en Costa Rica')
+st_folium(m, width=700, height=600)
